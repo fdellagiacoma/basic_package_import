97fce09ce45ba38a356415030e953834d6e80af5
fabrizio <fdellagiacoma@efs.at>
2023-03-29 16:33:39 +0200

    test

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+print(long_description)
+
 setuptools.setup(
     name='basicimport',
     version='0.0.3',
@@ -13,4 +15,4 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url='https://github.com/fdellagiacoma/basic_package_import',
     license='MIT',
-    packages=setuptools.find_packages(include=['basic_package_import']))
+    packages=setuptools.find_packages()
