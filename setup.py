a0cedda58e06e7ec6ce78de22f9c080e5e00ad32
fabrizio <fdellagiacoma@efs.at>
2023-03-29 16:15:20 +0200

    test

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='basicpackageimport',
+    name='basicimport',
     version='0.0.3',
     author='Santo padre',
     author_email='',
